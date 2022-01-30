b6dd81a58c254dca3339428a09ee07b09d0665fb
Rishu Kumar <60210505+rishu110067@users.noreply.github.com>
2022-01-30 11:47:45 +0530

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -1,3 +1,7 @@
+// strictly increasing - use lower_bound
+// increasing or non decreasing - use upper_bound
+
+
 ll LIS(vll &a)
 {
     ll n=a.size();
@@ -6,7 +10,8 @@ ll LIS(vll &a)
     rep(i,0,n)
     {
         ll j=ub(all(d),a[i])-d.begin();
-        if(d[j-1]<a[i] && a[i]<d[j]) d[j]=a[i];
+        // if(d[j-1]<a[i] && a[i]<d[j]) 
+            d[j]=a[i];
     }
     ll len=0;
     rep(i,1,n+1) if(d[i]<INF) len=i;
